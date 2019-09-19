64216c9cd769442d2435ca48f32a69bea89a836b
Adrian-Garcia <agarc5@hotmail.com>
2019-09-19 15:04:09 -0500

    added onChange and form value

--- client/src/Movies/EditForm.js
+++ client/src/Movies/EditForm.js
@@ -1,66 +1,62 @@
 import React, {useState, useEffect} from 'react';
 import Movie from './Movie';
+import axios from 'axios'
 
 
-const UpdateForm = props => {
-    const[props,setMovies] = useState({
+const UpdateForm = (props) => {
+
+    const[movies,setMovies] = useState({
         title: '',
         director: '',
         metascore: '',
         stars: []
 });
+  
 
 const handleChange = e => setMovies({ ...Movie, [e.target.name]: e.target.value })
 
-const handleStar = index => e => {
-    setMovies({...Movie, stars: Movie.stars.map((str, starIndex)=> {
-        if (starIndex === index) {
-            return e.target.value
-        } else {
-            return star;
-        }
-    })})
-}
 
 return(
-    
     <form>
         <input
         type="text"
         name="title"
         placeholder="title"
-        value={movie.title}
+        value={movies.title}
         onChange={handleChange}
         />
         <input 
-        type="text"
-        name="director"
-        placeholder="director"
-        value={movie.director}
-        onChange={handleChange}/>
+            type="text"
+            name="director"
+            placeholder="director"
+            value={movies.director}
+            onChange={handleChange}
+        />
 
         <input 
-        type="text"
-        name="metascore"
-        placeholder="metascore"
-        value={movie.title}
-        onChange={handleChange}/>
+            type="text"
+            name="metascore"
+            placeholder="metascore"
+            value={movies.title}
+            onChange={handleChange}
+            />
         {/* <input 
-        type="text"
-        name="title"
-        placeholder="title"
-        value={movie.title}
-        onChange={handleChange}/> */}
+            type="text"
+            name="title"
+            placeholder="title"
+            value={movie.title}
+            onChange={handleChange}/> */}
 
-        {Movie.stars.map((starName, index)=> {
-            return <input type="text"
+            {/* {Movie.stars.map((starName, index)=> {
+                return <input type="text"
                             placeholder="star"
+                            key={index}
                             value={starName}
                             onChange={handleStar(index)}
                             />
-        })}
+            })} */}
     </form>
     )
 }
 
-export default UpdateForm; 
+export default UpdateForm;
